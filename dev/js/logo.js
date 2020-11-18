7e4723d6b4f0aa2c420a95e1f39ad4205cb31edf
Marguerita Cruz-Urbanc <mmc399@drexel.edu>
2020-11-17 20:54:15 -0500

    initial logo animation

--- dev/js/logo.js
+++ dev/js/logo.js
@@ -1,1 +1,26 @@
-// import {gsap} from "gsap";
+import {gsap} from "gsap";
+import {DrawSVGPlugin} from "gsap/DrawSVGPlugin";
+
+gsap.registerPlugin(DrawSVGPlugin);
+
+// gsap.set("#outerCircle", {transformOrigin: "bottom"});
+
+const drawTL = gsap.timeline();
+export function drawAnimation(){
+
+    // drawTL.from("#outerCircle", {duration:2, ease: "none", drawSVG: "50% 50%"});
+    drawTL.fromTo("#outerCircle", {drawSVG: "0% 10%"},{duration:2, ease:"none", drawSVG: "90% 100%"}, "circleDraw")
+        .to("#outerCircle", {duration:2, ease:"none", drawSVG: "100%"}, "circleDraw2")
+        .to("#outerCircle", {duration:0.125, stroke: "#00ff7f"}, "lightUp");
+
+    drawTL.fromTo("#innerCircle", {drawSVG: "90% 100%"},{duration:2, ease:"none", drawSVG: "0% 10%"}, "circleDraw")
+        .to("#innerCircle", {duration:2, ease:"none", drawSVG: "100%"}, "circleDraw2")
+        .to("#innerCircle", {duration:0.125, stroke: "#89ff00"}, "lightUp");
+
+    drawTL.from("#topV", {duration:0.125, alpha:0}, "lightUp");
+    drawTL.from("#bottomW", {duration:0.125, alpha:0}, "lightUp");
+
+
+
+    return drawTL;
+}
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,2 +1,11 @@
-// import {gsap} from "gsap";
+import {gsap} from "gsap";
+import {GSDevTools} from "gsap/GSDevTools";
 
+import {drawAnimation} from "./logo.js";
+
+gsap.registerPlugin(GSDevTools);
+
+const mainTL = gsap.timeline()
+mainTL.add(drawAnimation());
+
+GSDevTools.create();
