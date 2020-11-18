2647636446e806bd1aad8765433627cdd45e7b74
Marguerita Cruz-Urbanc <mmc399@drexel.edu>
2020-11-18 11:13:43 -0500

    quick class edits

--- dev/js/logo.js
+++ dev/js/logo.js
@@ -3,17 +3,18 @@ import {DrawSVGPlugin} from "gsap/DrawSVGPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin);
 
-// gsap.set("#outerCircle", {transformOrigin: "bottom"});
+gsap.set("#outerCircle", {transformOrigin: "center"});
+gsap.set("#innerCircle", {transformOrigin: "center"});
 
 const drawTL = gsap.timeline();
 export function drawAnimation(){
 
     // drawTL.from("#outerCircle", {duration:2, ease: "none", drawSVG: "50% 50%"});
-    drawTL.fromTo("#outerCircle", {drawSVG: "0% 10%"},{duration:2, ease:"none", drawSVG: "90% 100%"}, "circleDraw")
+    drawTL.fromTo("#outerCircle", {drawSVG: "0% 10%"},{duration:2, ease:"none", rotate: 90, drawSVG: "90% 100%"}, "circleDraw")
         .to("#outerCircle", {duration:2, ease:"none", drawSVG: "100%"}, "circleDraw2")
         .to("#outerCircle", {duration:0.125, stroke: "#00ff7f"}, "lightUp");
 
-    drawTL.fromTo("#innerCircle", {drawSVG: "90% 100%"},{duration:2, ease:"none", drawSVG: "0% 10%"}, "circleDraw")
+    drawTL.fromTo("#innerCircle", {drawSVG: "90% 100%"},{duration:2, ease:"none", rotate: -90, drawSVG: "0% 10%"}, "circleDraw")
         .to("#innerCircle", {duration:2, ease:"none", drawSVG: "100%"}, "circleDraw2")
         .to("#innerCircle", {duration:0.125, stroke: "#89ff00"}, "lightUp");
 
