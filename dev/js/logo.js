3d6f067344bd0568be07693fa45f50ae41fcb586
Marguerita Cruz-Urbanc <mmc399@drexel.edu>
2020-11-22 14:23:33 -0500

    organizing designs and files

--- dev/js/logo.js
+++ dev/js/logo.js
@@ -1,27 +1,37 @@
 import {gsap} from "gsap";
 import {DrawSVGPlugin} from "gsap/DrawSVGPlugin";
+import {MorphSVGPlugin} from "gsap/MorphSVGPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin);
+gsap.registerPlugin(MorphSVGPlugin);
 
 gsap.set("#outerCircle", {transformOrigin: "center"});
 gsap.set("#innerCircle", {transformOrigin: "center"});
 
-const drawTL = gsap.timeline();
-export function drawAnimation(){
+const logoTL = gsap.timeline();
+export function logoAnimation(){
 
-    // drawTL.from("#outerCircle", {duration:2, ease: "none", drawSVG: "50% 50%"});
-    drawTL.fromTo("#outerCircle", {drawSVG: "0% 10%"},{duration:2, ease:"none", rotate: 90, drawSVG: "90% 100%"}, "circleDraw")
+    logoTL.fromTo("#outerCircle", {drawSVG: "0% 10%"},{duration:2, ease:"none", rotate: 90, drawSVG: "90% 100%"}, "circleDraw")
         .to("#outerCircle", {duration:2, ease:"none", drawSVG: "100%"}, "circleDraw2")
         .to("#outerCircle", {duration:0.125, stroke: "#00ff7f"}, "lightUp");
 
-    drawTL.fromTo("#innerCircle", {drawSVG: "90% 100%"},{duration:2, ease:"none", rotate: -90, drawSVG: "0% 10%"}, "circleDraw")
+    logoTL.fromTo("#innerCircle", {drawSVG: "90% 100%"},{duration:2, ease:"none", rotate: -90, drawSVG: "0% 10%"}, "circleDraw")
         .to("#innerCircle", {duration:2, ease:"none", drawSVG: "100%"}, "circleDraw2")
         .to("#innerCircle", {duration:0.125, stroke: "#89ff00"}, "lightUp");
 
-    drawTL.from("#topV", {duration:0.125, alpha:0}, "lightUp");
-    drawTL.from("#bottomW", {duration:0.125, alpha:0}, "lightUp");
+    logoTL.from("#morphCircle1", {duration:2, y:-300, ease: "none"}, "circleDraw2")
+            .to("#morphCircle1", {duration:2, morphSVG:"#topV", ease: "bounce.out"});
+
+    logoTL.from("#morphCircle2", {duration:2, y:300, ease:"none"}, "circleDraw2")
+            .to("#morphCircle2", {duration:2, morphSVG:"#bottomW", ease:"bounce.out"});
+
+    // logoTL.to("#morphCircle1", {duration:2, morphSVG:"#topV"});
+    // logoTL.to("#morphCircle2", {duration:2, morphSVG:"#bottomW"});
+
+    // logoTL.from("#topV", {duration:0.125, alpha:0}, "lightUp");
+    // logoTL.from("#bottomW", {duration:0.125, alpha:0}, "lightUp");
 
 
 
-    return drawTL;
+    return logoTL;
 }
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,11 +1,11 @@
 import {gsap} from "gsap";
 import {GSDevTools} from "gsap/GSDevTools";
 
-import {drawAnimation} from "./logo.js";
+import {logoAnimation} from "./logo.js";
 
 gsap.registerPlugin(GSDevTools);
 
 const mainTL = gsap.timeline()
-mainTL.add(drawAnimation());
+mainTL.add(logoAnimation());
 
 GSDevTools.create();
