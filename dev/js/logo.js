75456e2b52c2d79a2bae1bbbe5c2ad566d28af23
Marguerita Cruz-Urbanc <mmc399@drexel.edu>
2020-11-23 12:04:44 -0500

    in class edits

--- dev/js/logo.js
+++ dev/js/logo.js
@@ -9,6 +9,8 @@ MorphSVGPlugin.convertToPath("circle, rect, ellipse, line, polygon, polyline");
 
 gsap.set("#outerCircle", {transformOrigin: "center"});
 gsap.set("#innerCircle", {transformOrigin: "center"});
+gsap.set("#topV", {morphSVG:"#morphCircle1"});
+gsap.set("#bottomW", {morphSVG:"#morphCircle2"});
 
 const logoTL = gsap.timeline();
 export function logoAnimation(){
@@ -21,20 +23,26 @@ export function logoAnimation(){
         .to("#innerCircle", {duration:2, ease:"none", drawSVG: "100%"}, "circleDraw2")
         .to("#innerCircle", {duration:0.125, stroke: "#89ff00"}, "lightUp");
 
-    logoTL.from("#morphCircle1", {duration:2, y:-300, ease: "bounce.out"}, "circleDraw2")
-            .to("#morphCircle1", {duration:0.5, morphSVG:"#topV", fill:"#C9FF00"}, "logoMorph");
+    // logoTL.from("#morphCircle1", {duration:2, y:-300, ease: "bounce.out"}, "circleDraw2")
+    //         .to("#morphCircle1", {duration:0.5, morphSVG:"#topV", fill:"#C9FF00"}, "logoMorph");
 
-    logoTL.from("#morphCircle2", {duration:2, y:300, ease:"bounce.out"}, "circleDraw2")
+    // logoTL.from("#morphCircle2", {duration:2, y:300, ease:"bounce.out"}, "circleDraw2")
+    //         .to("#morphCircle2", {duration:0.5, morphSVG:"#bottomW", fill:"#C9FF00"}, "logoMorph");
+
+    logoTL.from("#morphCircle1", {duration:2, y:-300, ease: "bounce.out"}, "circleDraw2")
+            .to("#morphCircle1", {duration:0.5, morphSVG:"#topV-2", fill:"#C9FF00"}, "logoMorph");
+
+    logoTL.from("#morphCircle2", {duration:2, y:300, ease: "bounce.out"}, "circleDraw2")
             .to("#morphCircle2", {duration:0.5, morphSVG:"#bottomW", fill:"#C9FF00"}, "logoMorph");
 
     logoTL.to("#outerCircle", {duration:2, ease:"none", drawSVG: "0%"}, "+=1.5");
     logoTL.to("#innerCircle", {duration:2, ease:"none", drawSVG: "0%"});
 
-    logoTL.to("#topV", {duration:0.5, morphSVG:"#morphCircle1", ease: "bounce.out", fill:"#C9FF00"})
-        .to("#morphCircle1", {duration:0.5, y:-300, ease: "none"});
+   //logoTL.to("#topV", {duration:0.5, morphSVG:"#morphCircle1", ease: "bounce.out", fill:"#C9FF00"})
+          //.to("#topV", {duration:0.5, y:-300, ease: "none"});
 
-    logoTL.to("#bottomW", {duration:0.5, morphSVG:"#morphCircle2", ease: "bounce.out", fill:"#C9FF00"})
-        .to("#morphCircle2", {duration:0.5, y:300, ease: "none"});
+ //logoTL.to("#bottomW", {duration:0.5, morphSVG:"#morphCircle2", ease: "bounce.out", fill:"#C9FF00"})
+        //.to("#morphCircle2", {duration:0.5, y:300, ease: "none"});
 
     // logoTL.to("#topV", {duration:2, morphSVG:"#morphCircle1"});
     // logoTL.to("#bottomW", {duration:2, morphSVG:"#morphCircle1"});
